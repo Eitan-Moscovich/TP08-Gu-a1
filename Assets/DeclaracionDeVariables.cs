fba5cc045a37a11ec07e7524af6a7c6f70eb42b6
Eitan-Moscovich <49703153@est.ort.edu.ar>
2026-04-16 10:49:24 -0300

    Initial commit

---
+++ Assets/DeclaracionDeVariables.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    int numero = 10;
+    float decimalNum = 3.14f;
+    string texto = "Hola mundo";
+    bool esVerdadero = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(numero);
+        Debug.Log(decimalNum);
+        Debug.Log(texto);
+        Debug.Log(esVerdadero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
